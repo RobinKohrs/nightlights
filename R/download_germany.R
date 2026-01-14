a1459c3afb7053599352ea5b998111e15c4e2696
Robin Kohrs <robin.kohrs@gmx.de>
2026-01-14 13:34:34 +0100

    new images

--- R/download_germany.R
+++ R/download_germany.R
@@ -20,8 +20,14 @@ roi_sf <- gadm(country = "DEU", level = 1, path = tempdir())
 
 
 ### Monthly data: raster for Dec 25
-r_202110 <- bm_raster(
+res <- bm_raster(
   roi_sf = roi_sf,
   product_id = "VNP46A3",
-  date = "2025-12-01",
+  date = "2025-11-01",
   bearer = bearer)
+
+# ++++++++++++++++++++++++++++++
+# write out ----
+# ++++++++++++++++++++++++++++++
+path_out  <- sys_make_path(here("data/geodata/nov_25_germany.tif"))
+writeRaster(res, path_out)
